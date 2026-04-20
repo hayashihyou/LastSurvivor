92c69ac7d000b4fe3b4373e020a8fccffd2e84b1
HayashiHyo <CA01244023@st.kawahara.ac.jp>
2026-04-20 20:05:23 +0900

    シーン遷移の実装 (#4)

---
+++ Assets/Project/Scripts/ResultScene.cs
@@ -0,0 +1,69 @@
+namespace LastSurvivor
+{
+    using Cysharp.Threading.Tasks;
+    using R3;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using DG.Tweening;
+    using TMPro;
+
+    /// <summary>
+    /// リザルト画面を管理するスクリプト
+    /// </summary>
+    public class ResultScene : MonoBehaviour
+    {
+        [Header("リトライボタン"),SerializeField] 
+        private Button retryButton;
+
+        [Header("タイトルボタン"),SerializeField] 
+        private Button titleButton;
+
+        [Header("スコアテキスト"),SerializeField] 
+        private TextMeshProUGUI scoreText;
+
+        /// <summary>
+        /// シーン開始時の処理
+        /// </summary>
+        void Start()
+        {
+            // スコアを取得
+            var score = PlayerPrefs.GetInt("Score", 0);
+            scoreText.text = $"Score: {score}";
+
+            //DOTweenを使ってスコアテキストをアニメーション表示
+            scoreText.transform.DOScale(1.2f, 0.3f).SetEase(Ease.OutBack);
+
+            // リトライボタンのクリックイベント
+            retryButton.onClick.AsObservable()
+                .Subscribe(_ => OnRetryClicked().Forget())
+                .AddTo(this);
+
+            // タイトルボタンのクリックイベント
+            titleButton.onClick.AsObservable()
+                .Subscribe(_ => OnTitleClicked().Forget())
+                .AddTo(this);
+        }
+
+        /// <summary>
+        /// リトライボタンをクリックした時の処理
+        /// </summary>
+        private async UniTask OnRetryClicked()
+        {
+            // ボタンを無効化して多重クリックを防止
+            retryButton.interactable = false;
+            await SceneLoader.Instance.LoadSceneAsync(SceneNameConstants.InGame);
+        }
+
+        /// <summary>
+        /// タイトルボタンをクリックしたときの処理
+        /// </summary>
+        private async UniTask OnTitleClicked()
+        {
+            // ボタンを無効化して多重クリックを防止
+            titleButton.interactable = false;
+
+            // タイトルシーンに遷移
+            await SceneLoader.Instance.LoadSceneAsync(SceneNameConstants.Title);
+        }
+    }
+}
---
+++ Assets/Project/Scripts/SceneNameConstants.cs
@@ -0,0 +1,14 @@
+namespace LastSurvivor
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// シーン名を定数で管理するクラス
+    /// </summary>
+    public class SceneNameConstants : MonoBehaviour
+    {
+        public static readonly string Title = "Title";
+        public static readonly string InGame = "InGame";
+        public static readonly string Result = "Result";
+    }
+}
