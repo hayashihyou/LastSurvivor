3d16fa2134a1d065bb4acb799d7eb11c2a12eb20
HayashiHyo <CA01244023@st.kawahara.ac.jp>
2026-04-25 15:44:00 +0900

    Hayashi/gun (#7)

---
+++ Assets/Project/Scripts/BulletController.cs
@@ -0,0 +1,51 @@
+﻿namespace LastSurvivor
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// 弾の発射を制御するクラス
+    /// </summary>
+    public class BulletController : MonoBehaviour
+    {
+        [Header("設定"),SerializeField]
+        public GameObject BulletPrefab;
+
+        [Header("CloneのBulletを格納する親オブジェクト"),SerializeField]
+        private GameObject _bullets;
+
+        [Header("マズルフラッシュのエフェクト"),SerializeField]
+        private ParticleSystem _muzzleFlash;
+
+        [Header("発射位置"),SerializeField]
+        private Transform _firePoint;
+
+        [Header("弾の速度"),SerializeField]
+        private float _bulletSpeed = 50f;
+
+        [Header("弾の寿命"),SerializeField]
+        private float _bulletLifetime = 5f;
+        /// <summary>
+        /// 弾を発射するタスク
+        /// </summary>
+        public void Shoot()
+        {
+            // マズルフラッシュエフェクトを再生
+            _muzzleFlash.Play(); 
+
+            // 弾のプレハブをインスタンス化して発射位置と回転を設定
+            var bullet = Instantiate(BulletPrefab, _firePoint.position, _firePoint.rotation, _bullets.transform);
+
+            // 弾のRigidbodyコンポーネントを取得して、前方に速度を与える
+            var rb = bullet.GetComponent<Rigidbody>();
+
+            if(rb != null)
+            {
+                // 弾を前方に発射
+                rb.linearVelocity = _firePoint.forward * _bulletSpeed;
+            }
+
+            // 5秒後に弾を破壊してメモリを解放
+            Destroy(bullet, _bulletLifetime); 
+        }
+    }
+}
--- Assets/Project/Scripts/PlayerAnimator.cs
+++ Assets/Project/Scripts/PlayerAnimator.cs
@@ -17,14 +17,31 @@
         [Header("移動"), SerializeField]
         private PlayerMover _playerMover;
 
+        [Header("射撃設定"), SerializeField]
+        private BulletController _bulletController;
+
+        [Header("連射速度"), SerializeField]
+        private float _fireRate = 0.1f;
+
+        [Header("反動設定"), SerializeField]
+        private Transform _weaponPivot;
+
+        [Header("反動の強さ"), SerializeField]
+        private float _recoilForce = 0.05f;
+
+        [Header("元に戻る速さ"), SerializeField]
+        private float _returnSpeed = 5f;
+
+        // 武器の元の位置を保存する変数
+        private Vector3 _originalPosition;
+
         /// <summary>
         /// アニメーターのパラメータ
         /// </summary>
         public static readonly int IsRunning = Animator.StringToHash("isRunning");
         public static readonly int IsMoving = Animator.StringToHash("isMoving");
+        public static readonly int IsFiring = Animator.StringToHash("isFiring");
         public static readonly int IsDead = Animator.StringToHash("isDead");
-
-
 
         /// <summary>
         /// インスタンス化直後に呼び出される初期化処理
@@ -43,7 +60,20 @@
         /// </summary>
         void Start()
         {
+            _originalPosition = _weaponPivot.localPosition;
             SubscribeEvents();
+        }
+
+        void Update()
+        {
+            // 武器の位置を元に戻す
+            _weaponPivot.localPosition = Vector3.Lerp(_weaponPivot.localPosition, _originalPosition, Time.deltaTime * _returnSpeed);
+        }
+
+        void ApplyRecoil()
+        {
+            // 武器の位置を反動分だけ後ろに移動させる
+            _weaponPivot.localPosition -= new Vector3(0, 0, -_recoilForce);
         }
 
         /// <summary>
@@ -51,16 +81,41 @@
         /// </summary>
         private void SubscribeEvents()
         {
+            // プレイヤーの移動状態を購読し、アニメーターのisMovingパラメータを更新
             _playerMover.IsMoving
              .Subscribe(isMoving =>
                 _animator.SetBool(IsMoving, isMoving))
              .AddTo(this);
 
+            // プレイヤーの走行状態を購読し、アニメーターのisRunningパラメータを更新
             _playerMover.IsRunning
                 .Subscribe(isRunning =>
                     _animator.SetBool(IsRunning, isRunning))
                 .AddTo(this);
 
+            // 毎フレーム、射撃ボタンが押されているかとプレイヤーが死亡していないかをチェック
+            Observable.EveryUpdate()
+                    .Subscribe(_ =>
+                    {
+                        var isFiring = Input.GetButton("Fire1") && !_playerStatus.IsDead.Value;
+                        _animator.SetBool(IsFiring, isFiring);
+                    })
+                    .AddTo(this);
+
+
+            // 射撃ボタンが押されていて、プレイヤーが死亡していない場合に、一定の連射速度で弾を発射する
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetButton("Fire1") && !_playerStatus.IsDead.Value)
+                .ThrottleFirst(System.TimeSpan.FromSeconds(_fireRate),UnityTimeProvider.Update)
+                .Subscribe(_ =>
+                {
+                    _bulletController.Shoot();
+                    // 武器の反動を適用するタスク
+                    ApplyRecoil();
+                })
+                .AddTo(this);
+
+            // プレイヤーの死亡状態を購読し、アニメーターのisDeadパラメータを更新
             _playerStatus.IsDead
                 .Subscribe(isDead =>
                     _animator.SetBool(IsDead, isDead))
