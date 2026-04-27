185f493581dfe70f23a4b10cff4c78201d7f88f3
HayashiHyo <CA01244023@st.kawahara.ac.jp>
2026-04-27 23:33:18 +0900

    クロスヘアの実装

---
+++ Assets/Project/Scripts/Crosshair.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    [Header("見た目")]
+    public float lineLength = 20f;
+    public float lineThickness = 2f;
+    public Color crosshairColor = Color.white;
+
+    private void Start()
+    {
+        CreateCrosshair();  
+    }
+
+    void CreateCrosshair()
+    {
+        CreateLine("Horizontal", new Vector2(lineLength, lineThickness));
+
+        CreateLine("Vertical", new Vector2(lineThickness, lineLength));
+    }
+
+    void CreateLine(string name, Vector2 size)
+    {
+        var go = new GameObject(name, typeof(RectTransform), typeof(CanvasRenderer), typeof(UnityEngine.UI.Image));
+        go.transform.SetParent(transform, false);
+
+        var rt =go.GetComponent<RectTransform>();
+        rt.sizeDelta = size;
+
+        go.GetComponent<UnityEngine.UI.Image>().color = crosshairColor;
+    }
+}
